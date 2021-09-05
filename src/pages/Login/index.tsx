38987312dc38f8c99d31b466385a8f9550c0503c
Damian Soria <damian.soria@engworks.com>
2021-09-04 22:02:49 -0300

    fix: styles login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,5 @@
 import {
- Box, Container, Grid, Paper, TextField,
+ Container, Grid, TextField,
 } from '@material-ui/core';
 import { Helmet } from 'react-helmet-async';
 import { useSnackbar, VariantType } from 'notistack';
@@ -7,7 +7,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { useHistory } from 'react-router';
 import { AuthApi } from '../../service/Auth';
 import { Title, Button } from '../../shared/styles';
-import { Header } from './styles';
+import { Box, Header } from './styles';
 
 type Inputs = {
     inputName: string,
@@ -42,45 +42,47 @@ const Login: React.FC = ():JSX.Element => {
         <Helmet>
           <title>Sign In</title>
         </Helmet>
-        <Container>
+        <Container maxWidth="sm">
           <Header>
             <Title>
               Sign In
             </Title>
           </Header>
-          <Container>
-            <Paper>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <Grid container xs={6} spacing={2} justifyContent="center" alignItems="center">
-                  <Grid item xs={12}>
-                    <TextField
-                      fullWidth
-                      id="username"
-                      label="Username"
-                      type="text"
-                      variant="outlined"
-                      {...register('inputName', { required: true })}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      fullWidth
-                      id="password"
-                      label="Password"
-                      type="password"
-                      variant="outlined"
-                      {...register('inputPassword', { required: true })}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Button type="submit">
-                      Sign in
-                    </Button>
-                  </Grid>
-                </Grid>
-              </form>
-            </Paper>
-          </Container>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid
+              container
+              spacing={3}
+              direction="column"
+              alignItems="center"
+              justify="center"
+            >
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  type="text"
+                  variant="outlined"
+                  {...register('inputName', { required: true })}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                  {...register('inputPassword', { required: true })}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button type="submit">
+                  Sign in
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
         </Container>
       </Box>
     );
